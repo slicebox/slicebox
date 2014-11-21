4a79ac1978472517032bd2dd9e2f7173604a17b2
Karl Sjöstrand <karl.sjostrand@med.lu.se>
2014-11-21 19:51:42 +0100

    After much agony, now also tests a PUT request

--- src/test/scala/se/vgregion/app/RestTest.scala
+++ src/test/scala/se/vgregion/app/RestTest.scala
@@ -5,10 +5,17 @@ import spray.testkit.ScalatestRouteTest
 import spray.routing.HttpService
 import spray.http.StatusCodes._
 import spray.routing.HttpServiceActor
+import se.vgregion.filesystem.FileSystemProtocol._
+import spray.http.ContentTypes._
+import spray.httpx.marshalling.Marshaller
+import spray.http.HttpEntity
+import spray.http.HttpRequest
+import java.nio.file.Files
+import java.nio.file.FileSystems
 
 class RestTest extends FlatSpec with Matchers with ScalatestRouteTest with RestApi {
   def actorRefFactory = system // connect the DSL to the test ActorSystem
-  
+
   "The service" should "return 200 OK when asking for all metadata" in {
     Get("/metadata/list") ~> routes ~> check {
       status should be(OK)
@@ -20,5 +27,18 @@ class RestTest extends FlatSpec with Matchers with ScalatestRouteTest with RestA
       status should be(NotFound)
     }
   }
-  
+
+  it should "return a monitoring message when asked to monitor a new directory" in {
+    val tempDir = Files.createTempDirectory("akka-dcm-temp-dir-").toString().replace("\\", "/")
+    val monitorDir = MonitorDir(tempDir)
+
+    implicit val monitorDirMarshaller = Marshaller.of[MonitorDir](`application/json`) {
+      (value, ct, ctx) => ctx.marshalTo(HttpEntity(ct, s"""{ "directory": "${monitorDir.directory}" }"""))
+    }
+    
+    Put("/monitordirectory", monitorDir) ~> routes ~> check {
+      responseAs[String] should be(s"Now monitoring directory ${monitorDir.directory}")
+    }
+  }
+
 }
