89309926721fab4bc279281a105641123b150252
Mattias Hagstrand <mattias.hagstrand@hitude.com>
2015-01-28 13:21:37 +0100

    Added patients list to home view.

--- src/main/assets/js/home.js
+++ src/main/assets/js/home.js
@@ -12,5 +12,29 @@ angular.module('slicebox.home', ['ngRoute'])
 })
 
 .controller('HomeCtrl', function($scope, $http) {
-  
+    // Initialization
+    $scope.callbacks = {};
+
+    $scope.uiState = {
+        errorMessage: null
+    };
+
+    // Scope functions
+    $scope.loadPatients = function(startIndex, count, orderByProperty, orderByDirection) {
+        return $http.get('/api/metadata/patients');
+    };
+
+    // $scope.convertPatientsPageData = function(patients) {
+    //     angular.forEach(patients, function(patient) {
+    //         angular.forEach(patient, function(propertyValue, propertyName) {
+    //             patient[propertyName] = propertyValue.value;
+    //         });
+    //     });
+
+    //     return patients;
+    // };
+
+    $scope.closeErrorMessageAlert = function() {
+        $scope.uiState.errorMessage = null;
+    };
 });
