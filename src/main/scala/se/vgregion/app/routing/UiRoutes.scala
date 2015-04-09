03bbf1553ff69407557000af1db0f51f53374102
Karl Sjöstrand <karl.sjostrand@med.lu.se>
2015-04-09 16:01:46 +0200

    A few more favicon routes

--- src/main/scala/se/vgregion/app/routing/UiRoutes.scala
+++ src/main/scala/se/vgregion/app/routing/UiRoutes.scala
@@ -36,52 +36,62 @@ trait UiRoutes { this: RestApi =>
         }
       }
     }
-  
+
   def faviconRoutes: Route =
     path("apple-touch-icon-57x57.png") {
       getFromResource("public/images/favicons/apple-touch-icon-57x57.png")
-    } ~
-    path("apple-touch-icon-60x60.png") {
+    } ~ path("apple-touch-icon-60x60.png") {
       getFromResource("public/images/favicons/apple-touch-icon-60x60.png")
-    } ~
-    path("apple-touch-icon-72x72.png") {
+    } ~ path("apple-touch-icon-72x72.png") {
       getFromResource("public/images/favicons/apple-touch-icon-72x72.png")
-    } ~
-    path("apple-touch-icon-76x76.png") {
+    } ~ path("apple-touch-icon-76x76.png") {
       getFromResource("public/images/favicons/apple-touch-icon-76x76.png")
-    } ~
-    path("apple-touch-icon-114x114.png") {
+    } ~ path("apple-touch-icon-114x114.png") {
       getFromResource("public/images/favicons/apple-touch-icon-114x114.png")
-    } ~
-    path("apple-touch-icon-120x120.png") {
+    } ~ path("apple-touch-icon-120x120.png") {
       getFromResource("public/images/favicons/apple-touch-icon-120x120.png")
-    } ~
-    path("apple-touch-icon-144x144.png") {
+    } ~ path("apple-touch-icon-144x144.png") {
       getFromResource("public/images/favicons/apple-touch-icon-144x144.png")
-    } ~
-    path("apple-touch-icon-152x152.png") {
+    } ~ path("apple-touch-icon-152x152.png") {
       getFromResource("public/images/favicons/apple-touch-icon-152x152.png")
-    } ~
-    path("apple-touch-icon-180x180.png") {
+    } ~ path("apple-touch-icon-180x180.png") {
       getFromResource("public/images/favicons/apple-touch-icon-180x180.png")
-    } ~
-    path("favicon-32x32.png") {
-      getFromResource("public/images/favicons/favicon-32x32.png")
-    } ~
-    path("favicon-194x194.png") {
-      getFromResource("public/images/favicons/favicon-194x194.png")
-    } ~
-    path("favicon-96x96.png") {
-      getFromResource("public/images/favicons/favicon-96x96.png")
-    } ~
-    path("android-chrome-192x192.png") {
-      getFromResource("public/images/favicons/android-chrome-192x192.png")
-    } ~
-    path("favicon-16x16.png") {
+    } ~ path("favicon.ico") {
+      getFromResource("public/images/favicons/favicon.ico")
+    } ~ path("favicon-16x16.png") {
       getFromResource("public/images/favicons/favicon-16x16.png")
-    } ~
-    path("mstile-144x144.png") {
+    } ~ path("favicon-32x32.png") {
+      getFromResource("public/images/favicons/favicon-32x32.png")
+    } ~ path("favicon-96x96.png") {
+      getFromResource("public/images/favicons/favicon-96x96.png")
+    } ~ path("favicon-194x194.png") {
+      getFromResource("public/images/favicons/favicon-194x194.png")
+    } ~ path("android-chrome-36x36.png") {
+      getFromResource("public/images/favicons/android-chrome-36x36.png")
+    } ~ path("android-chrome-48x48.png") {
+      getFromResource("public/images/favicons/android-chrome-48x48.png")
+    } ~ path("android-chrome-72x72.png") {
+      getFromResource("public/images/favicons/android-chrome-72x72.png")
+    } ~ path("android-chrome-96x96.png") {
+      getFromResource("public/images/favicons/android-chrome-96x96.png")
+    } ~ path("android-chrome-144x144.png") {
+      getFromResource("public/images/favicons/android-chrome-144x144.png")
+    } ~ path("android-chrome-192x192.png") {
+      getFromResource("public/images/favicons/android-chrome-192x192.png")
+    } ~ path("mstile-70x70.png") {
+      getFromResource("public/images/favicons/mstile-70x70.png")
+    } ~ path("mstile-144x144.png") {
       getFromResource("public/images/favicons/mstile-144x144.png")
+    } ~ path("mstile-150x150.png") {
+      getFromResource("public/images/favicons/mstile-150x150.png")
+    } ~ path("mstile-310x150.png") {
+      getFromResource("public/images/favicons/mstile-310x150.png")
+    } ~ path("mstile-310x310.png") {
+      getFromResource("public/images/favicons/mstile-310x310.png")
+    } ~ path("manifest.json") {
+      getFromResource("public/images/favicons/manifest.json")
+    } ~ path("browserconfig.xml") {
+      getFromResource("public/images/favicons/browserconfig.xml")
     }
 
 }
