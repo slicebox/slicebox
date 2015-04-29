f5e6c19ffe9b64b92f0976c8e92deca329febf48
Anton Sörensen <anton.sorensen@gmail.com>
2015-04-29 15:30:44 +0200

    revert because wrong branch

--- src/main/matlab/authprompt.m
+++
@@ -1,9 +0,0 @@
-function [username, password] = authprompt() 
-  prompt = {'Username:','Password:'};
-  dlg_title = 'Input';
-  num_lines = 1;
-  def = {'',''};
-  answer = inputdlg(prompt,dlg_title,num_lines,def);
-  username = answer{1};
-  password = answer{2};
-end
--- src/main/matlab/sbxgetimageinfo.m
+++
@@ -1,5 +0,0 @@
-function datasets = sbxgetimageinfo(seriesid, sbdata)
-% Will fetch information about the dicom images of a series. The results
-% are needed when using sbreadimage.
-url = [sbdata.url, '/api/metadata/images?seriesid=',num2str(seriesid)];
-datasets = webread(url, sbdata.weboptions);
