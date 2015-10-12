11584b8be3b14cac44f212ba2bc68dc73a41d70b
Karl Sjöstrand <karl.sjostrand@gmail.com>
2015-10-12 09:15:30 +0200

    Attempts to get SSL to work instead of encrypting of datasets only

--- src/main/scala/se/nimsa/sbx/app/Main.scala
+++ src/main/scala/se/nimsa/sbx/app/Main.scala
@@ -19,19 +19,16 @@ package se.nimsa.sbx.app
 import scala.concurrent.duration.DurationInt
 import scala.util.Failure
 import scala.util.Success
-
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
-
 import spray.can.Http
-
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
-object Main extends App with LazyLogging {
+object Main extends App with LazyLogging with SslConfiguration {
   val config = ConfigFactory.load()
   val host = config.getString("http.host")
   val port = config.getInt("http.port")
@@ -40,7 +37,6 @@ object Main extends App with LazyLogging {
 
   implicit val timeout = Timeout(70.seconds)
   implicit val ec = system.dispatcher
-  
   val api = system.actorOf(Props(new RestInterface()), "httpInterface")
   IO(Http).ask(Http.Bind(listener = api, interface = host, port = port)) onComplete {
     case Success(message) =>
