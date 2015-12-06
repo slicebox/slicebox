caa8a211f25b23938df694e49935808387a26c45
Karl Sjöstrand <karl.sjostrand@gmail.com>
2015-12-06 14:21:25 +0100

    Added (failing) test for adding multiple images associated with the same anon key

--- src/test/scala/se/nimsa/sbx/anonymization/AnonymizationServiceActorTest.scala
+++ src/test/scala/se/nimsa/sbx/anonymization/AnonymizationServiceActorTest.scala
@@ -1,11 +1,9 @@
 package se.nimsa.sbx.anonymization
 
 import java.util.Date
-
 import scala.slick.driver.H2Driver
 import scala.slick.jdbc.JdbcBackend.Database
 import scala.concurrent.duration.DurationInt
-
 import org.dcm4che3.data.Attributes
 import org.dcm4che3.data.Tag
 import org.dcm4che3.data.VR
@@ -13,19 +11,20 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.Matchers
 import org.scalatest.WordSpecLike
-
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.actor.actorRef2Scala
 import akka.testkit.ImplicitSender
 import akka.testkit.TestKit
-
 import se.nimsa.sbx.app.DbProps
-
 import se.nimsa.sbx.util.TestUtil._
-
+import se.nimsa.sbx.dicom.DicomUtil.datasetToImage
 import AnonymizationProtocol._
 import AnonymizationUtil.anonymizeDataset
+import se.nimsa.sbx.dicom.DicomHierarchy.Image
+import akka.actor.Actor
+import se.nimsa.sbx.metadata.MetaDataProtocol.GetImage
+import se.nimsa.sbx.app.GeneralProtocol.ImageAdded
 
 class AnonymizationServiceActorTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
     with WordSpecLike with Matchers with BeforeAndAfterAll with BeforeAndAfterEach {
@@ -43,6 +42,18 @@ class AnonymizationServiceActorTest(_system: ActorSystem) extends TestKit(_syste
 
   val anonymizationService = system.actorOf(Props(new AnonymizationServiceActor(dbProps, 5.minutes)), name = "AnonymizationService")
 
+  case class AddImage(image: Image)
+  val metaDataService = system.actorOf(Props(new Actor {
+    var addedImages = Seq.empty[Image]
+    def receive = {
+      case AddImage(image) =>
+        addedImages = addedImages :+ image
+        sender ! ImageAdded(image, null)
+      case GetImage(imageId) =>
+        sender ! addedImages.find(_.id == imageId)
+    }
+  }), name = "MetaDataService")
+  
   override def afterEach() =
     db.withSession { implicit session =>
       anonymizationDao.clear
@@ -95,6 +106,35 @@ class AnonymizationServiceActorTest(_system: ActorSystem) extends TestKit(_syste
             reversed.getString(Tag.FrameOfReferenceUID) should be(key.frameOfReferenceUID)
         }
       }
+    }
+    
+    "add anonymization image records to an anonymization key when several datasets from the same series are anonymized" in {
+      db.withSession { implicit session =>
+        val dataset1 = createDataset()
+        val dataset2 = new Attributes(dataset1)
+        val dataset3 = new Attributes(dataset1)
+        dataset1.setString(Tag.SOPInstanceUID, VR.UI, "sopuid1")
+        dataset2.setString(Tag.SOPInstanceUID, VR.UI, "sopuid2")
+        dataset3.setString(Tag.SOPInstanceUID, VR.UI, "sopuid3")
+        val image1 = datasetToImage(dataset1)
+        val image2 = datasetToImage(dataset2)
+        val image3 = datasetToImage(dataset3)
+        metaDataService ! AddImage(image1)
+        expectMsgType[ImageAdded]
+        metaDataService ! AddImage(image2)
+        expectMsgType[ImageAdded]
+        metaDataService ! AddImage(image3)
+        expectMsgType[ImageAdded]
+        anonymizationService ! Anonymize(image1.id, dataset1, Seq.empty)
+        expectMsgType[Attributes]
+        anonymizationService ! Anonymize(image2.id, dataset2, Seq.empty)
+        expectMsgType[Attributes]
+        anonymizationService ! Anonymize(image3.id, dataset3, Seq.empty)
+        expectMsgType[Attributes]
+        val anonImages = anonymizationDao.listAnonymizationKeyImages
+        anonImages should have length 3
+        anonImages.map(_.imageId) shouldBe List(image1.id, image2.id, image3.id)
+      }      
     }
   }
 
