1eec1442f1fb035b719c38762cd3c401c877a61a
Karin Toth <karin.toth@exini.com>
2016-03-08 17:24:41 +0100

    Started work on import session routes

---
+++ src/main/scala/se/nimsa/sbx/app/routing/ImportRoutes.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2016 Lars Edenbrandt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package se.nimsa.sbx.app.routing
+
+import java.io.File
+
+import scala.concurrent.Future
+
+import org.dcm4che3.data.Attributes
+
+import akka.pattern.ask
+import se.nimsa.sbx.anonymization.AnonymizationProtocol._
+import se.nimsa.sbx.anonymization.AnonymizationUtil
+import se.nimsa.sbx.app.GeneralProtocol._
+import se.nimsa.sbx.app.SliceboxService
+import se.nimsa.sbx.dicom.DicomHierarchy.Image
+import se.nimsa.sbx.dicom.DicomUtil
+import se.nimsa.sbx.dicom.ImageAttribute
+import se.nimsa.sbx.storage.StorageProtocol._
+import se.nimsa.sbx.user.UserProtocol.ApiUser
+import spray.http.ContentType.apply
+import spray.http.FormFile
+import spray.http.HttpData
+import spray.http.HttpEntity
+import spray.http.HttpHeaders._
+import spray.http.MediaTypes._
+import spray.http.StatusCodes._
+import spray.routing.Route
+import spray.routing.directives._
+
+trait ImportRoutes { this: SliceboxService =>
+
+  def importRoutes(apiUser: ApiUser): Route =
+    pathPrefix("importing") {
+      path("session") {
+        pathEndOrSingleSlash {
+          get {
+            complete(OK)
+          } ~ post {
+            complete(Created)
+          } ~ delete {
+            complete(OK)
+          }
+
+        }
+      }
+    }
+
+}
---
+++ src/test/scala/se/nimsa/sbx/app/routing/ImportRoutesTest.scala
@@ -0,0 +1,54 @@
+package se.nimsa.sbx.app.routing
+
+import java.io.File
+import scala.slick.driver.H2Driver
+import org.dcm4che3.data.Tag
+import org.dcm4che3.data.VR
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import se.nimsa.sbx.anonymization.AnonymizationDAO
+import se.nimsa.sbx.anonymization.AnonymizationProtocol._
+import se.nimsa.sbx.dicom.DicomHierarchy._
+import se.nimsa.sbx.dicom.DicomUtil
+import se.nimsa.sbx.dicom.ImageAttribute
+import se.nimsa.sbx.util.TestUtil
+import spray.http.BodyPart
+import spray.http.ContentTypes
+import spray.http.HttpData
+import spray.http.MultipartFormData
+import spray.http.StatusCodes._
+import spray.httpx.SprayJsonSupport._
+import spray.httpx.unmarshalling.BasicUnmarshallers.ByteArrayUnmarshaller
+import se.nimsa.sbx.metadata.MetaDataDAO
+
+class ImportRoutesTest extends FlatSpec with Matchers with RoutesTestBase {
+
+  override def dbUrl() = "jdbc:h2:mem:imageroutestest;DB_CLOSE_DELAY=-1"
+
+  val metaDataDao = new MetaDataDAO(H2Driver)
+
+  override def afterEach() {
+    db.withSession { implicit session =>
+      metaDataDao.clear
+    }
+  }
+
+  "Import routes" should "return 201 Created when calling post" in {
+    PostAsUser("/importing/session") ~> routes ~> check {
+      status should be(Created)
+    }
+  }
+
+  it should "return 200 Created when calling get" in {
+    GetAsUser("/importing/session") ~> routes ~> check {
+      status should be(OK)
+    }
+  }
+
+  it should "return 200 Created when calling delete" in {
+    DeleteAsUser("/importing/session") ~> routes ~> check {
+      status should be(OK)
+    }
+  }
+
+}
