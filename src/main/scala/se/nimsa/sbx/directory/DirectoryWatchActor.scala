d2356e8df2412f778b14410ab4faffb9803ee768
Karl Sjöstrand <karl.sjostrand@gmail.com>
2017-05-30 18:05:14 -0400

    Recursive directory watch using single fs watch

--- src/main/scala/se/nimsa/sbx/directory/DirectoryWatchActor.scala
+++ src/main/scala/se/nimsa/sbx/directory/DirectoryWatchActor.scala
@@ -23,7 +23,7 @@ import akka.event.LoggingReceive
 import akka.pattern.ask
 import akka.stream.alpakka.file.DirectoryChange
 import akka.stream.alpakka.file.scaladsl.{Directory, DirectoryChangesSource}
-import akka.stream.scaladsl.{FileIO, Keep, Sink}
+import akka.stream.scaladsl.{FileIO, Keep, Sink, Source => StreamSource}
 import akka.stream.{ActorMaterializer, KillSwitches, UniqueKillSwitch}
 import akka.util.Timeout
 import org.dcm4che3.data.Attributes
@@ -61,41 +61,49 @@ class DirectoryWatchActor(watchedDirectory: WatchedDirectory,
 
   val fs = FileSystems.getDefault
 
-  val source = Directory.ls(fs.getPath(watchedDirectory.path)).map(path => (path, DirectoryChange.Creation))
-    .concat(DirectoryChangesSource(fs.getPath(watchedDirectory.path), pollInterval = 5.seconds, maxBufferSize = 100000))
-    .filter {
-      case (_, change) => change == DirectoryChange.Creation || change == DirectoryChange.Modification
-    }
-    .map(_._1)
-    .filter(path => !Files.isDirectory(path) && Files.isRegularFile(path))
-    .mapAsync(5) { path =>
-      val tempPath = DicomStreams.createTempPath()
-      FileIO.fromPath(path).runWith(dicomDataSink(storage.fileSink(tempPath), reverseAnonymizationQuery)).flatMap {
-        case (_, maybeDataset) =>
-          val attributes: Attributes = maybeDataset.getOrElse(throw new DicomStreamException("DICOM data has no dataset"))
-          metaDataService.ask(AddMetaData(attributes, sbxSource)).mapTo[MetaDataAdded].flatMap { metaData =>
-            storageService.ask(MoveDicomData(tempPath, s"${metaData.image.id}")).map { _ =>
-              system.eventStream.publish(ImageAdded(metaData.image, sbxSource, !metaData.imageAdded))
-            }
-          }
-      }.recover {
-        case NonFatal(e) =>
-          SbxLog.error("Directory", s"Could not add file ${Paths.get(watchedDirectory.path).relativize(path)}: ${e.getMessage}")
+  val source =
+    Directory.walk(fs.getPath(watchedDirectory.path)).map(path => (path, DirectoryChange.Creation)) // recurse on startup
+      .concat(DirectoryChangesSource(fs.getPath(watchedDirectory.path), pollInterval = 5.seconds, maxBufferSize = 100000)) // watch
+      .filter {
+        case (_, change) => change == DirectoryChange.Creation || change == DirectoryChange.Modification
       }
-    }
-    .viaMat(KillSwitches.single)(Keep.right)
-    .toMat(Sink.last)(Keep.left)
+      .map(_._1)
+      .flatMapConcat { path =>
+        if (Files.isDirectory(path))
+          Directory.walk(path).filter(p => Files.isRegularFile(p)) // directory: recurse once
+        else if (Files.isRegularFile(path))
+          StreamSource.single(path) // regular file: pass
+        else
+          StreamSource.empty // other (symlinks etc): ignore
+      }
+      .mapAsync(5) { path => // do import
+        val tempPath = DicomStreams.createTempPath()
+        FileIO.fromPath(path).runWith(dicomDataSink(storage.fileSink(tempPath), reverseAnonymizationQuery)).flatMap {
+          case (_, maybeDataset) =>
+            val attributes: Attributes = maybeDataset.getOrElse(throw new DicomStreamException("DICOM data has no dataset"))
+            metaDataService.ask(AddMetaData(attributes, sbxSource)).mapTo[MetaDataAdded].flatMap { metaData =>
+              storageService.ask(MoveDicomData(tempPath, s"${metaData.image.id}")).map { _ =>
+                system.eventStream.publish(ImageAdded(metaData.image, sbxSource, !metaData.imageAdded))
+              }
+            }
+        }.recover {
+          case NonFatal(e) =>
+            SbxLog.error("Directory", s"Could not add file ${Paths.get(watchedDirectory.path).relativize(path)}: ${e.getMessage}")
+        }
+      }
+      .viaMat(KillSwitches.single)(Keep.right) // add a kill switch
+      .toMat(Sink.last)(Keep.left)
 
   var killSwitch: UniqueKillSwitch = _
 
   case object DicomDataProcessed
 
   override def preStart(): Unit = {
-    killSwitch = source.run()
+    killSwitch = source.run() // start watch process
   }
 
   override def postStop(): Unit = {
-    killSwitch.shutdown()
+    killSwitch.shutdown() // tear down watch process
   }
 
   override def callAnonymizationService[R: ClassTag](message: Any) = anonymizationService.ask(message).mapTo[R]
