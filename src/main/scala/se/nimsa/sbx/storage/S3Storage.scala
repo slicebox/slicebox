4cd201448eecdc1f4b9a635927c3ad01b38e9b48
Michael Kober <michael.kober@exini.com>
2017-08-04 10:26:12 +0200

    Use AWSSessionCredentials when using IAM roles, added some documentation to application.conf

--- src/main/scala/se/nimsa/sbx/storage/S3Storage.scala
+++ src/main/scala/se/nimsa/sbx/storage/S3Storage.scala
@@ -24,7 +24,7 @@ import akka.stream.alpakka.s3.scaladsl.S3Client
 import akka.stream.scaladsl.{Sink, Source}
 import akka.util.ByteString
 import akka.{Done, NotUsed}
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.auth.{AWSSessionCredentials, DefaultAWSCredentialsProviderChain}
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.DeleteObjectsRequest
 import com.amazonaws.{ClientConfiguration, Protocol}
@@ -45,18 +45,19 @@ class S3Storage(val bucket: String, val s3Prefix: String, val region: String)(im
   // Environment Variables - AWS_ACCESS_KEY_ID and AWS_SECRET_KEY
   // Java System Properties - aws.accessKeyId and aws.secretKey
   // Instance profile credentials delivered through the Amazon EC2 metadata service
-  val s3 = AmazonS3ClientBuilder.standard()
+  private val s3 = AmazonS3ClientBuilder.standard()
     .withRegion(region)
     .withCredentials(new DefaultAWSCredentialsProviderChain())
     .withClientConfiguration(new ClientConfiguration().withProtocol(Protocol.HTTPS))
     .build()
 
-  private val s3Stream = S3Client(credentialsFromProviderChain(), region)
-
   private def credentialsFromProviderChain() = {
     val providerChain = new DefaultAWSCredentialsProviderChain()
     val creds = providerChain.getCredentials
-    BasicCredentials(creds.getAWSAccessKeyId, creds.getAWSSecretKey)
+    creds match {
+      case sc: AWSSessionCredentials => akka.stream.alpakka.s3.auth.AWSSessionCredentials(sc.getAWSAccessKeyId, sc.getAWSSecretKey, sc.getSessionToken)
+      case _ => BasicCredentials(creds.getAWSAccessKeyId, creds.getAWSSecretKey)
+    }
   }
 
   private def s3Id(imageName: String): String = s3Prefix + "/" + imageName
@@ -77,10 +78,10 @@ class S3Storage(val bucket: String, val s3Prefix: String, val region: String)(im
     }
 
   override def fileSink(name: String)(implicit executionContext: ExecutionContext): Sink[ByteString, Future[Done]] =
-    s3Stream.multipartUpload(bucket, name).mapMaterializedValue(_.map(_ => Done))
+    S3Client(credentialsFromProviderChain(), region).multipartUpload(bucket, name).mapMaterializedValue(_.map(_ => Done))
 
   override def fileSource(imageId: Long): Source[ByteString, NotUsed] =
-    s3Stream.download(bucket, s3Id(imageName(imageId))).mapError {
+    S3Client(credentialsFromProviderChain(), region).download(bucket, s3Id(imageName(imageId))).mapError {
       // we do not have access to http status code here so not much we can do but map everything to NotFound
       case e: S3Exception => new NotFoundException(s"Data could not be transferred for image id $imageId: ${e.getMessage}")
     }
