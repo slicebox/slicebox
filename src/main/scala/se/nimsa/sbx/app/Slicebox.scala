75a9a4c6a76e5b31fad19306c7c6c7e8c69d8468
Karl Sjöstrand <karl.sjostrand@gmail.com>
2017-11-10 15:40:31 -0500

    post-release version bump

--- src/main/scala/se/nimsa/sbx/app/Slicebox.scala
+++ src/main/scala/se/nimsa/sbx/app/Slicebox.scala
@@ -52,7 +52,7 @@ import scala.util.{Failure, Success}
 
 trait SliceboxBase extends SliceboxRoutes with DicomStreamOps with JsonFormats with PlayJsonSupport {
 
-  val systemInformation: SystemInformation = SystemInformation("1.3")
+  val systemInformation: SystemInformation = SystemInformation("1.4-SNAPSHOT")
 
   val appConfig: Config  = ConfigFactory.load()
   val sliceboxConfig = appConfig.getConfig("slicebox")
