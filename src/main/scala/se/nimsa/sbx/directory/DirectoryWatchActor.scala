24c509362397c042aaf0d413dd93dc8bb395310d
Rickard Cronholm <rickard.cronholm@spectronic.se>
2018-08-29 13:47:44 +0200

    Delete file from watched directory after successfull import

--- src/main/scala/se/nimsa/sbx/directory/DirectoryWatchActor.scala
+++ src/main/scala/se/nimsa/sbx/directory/DirectoryWatchActor.scala
@@ -69,9 +69,10 @@ class DirectoryWatchActor(watchedDirectory: WatchedDirectory,
         else
           StreamSource.empty // other (symlinks etc): ignore
       }
-      .mapAsync(5) { path => // do import
+      .mapAsync(5) { path => // do import, delete if successful (might be permission issues here)
         storeDicomData(FileIO.fromPath(path), sbxSource, storage, Contexts.imageDataContexts, reverseAnonymization = true).map { metaData =>
-          system.eventStream.publish(ImageAdded(metaData.image.id, sbxSource, !metaData.imageAdded))
+        system.eventStream.publish(ImageAdded(metaData.image.id, sbxSource, !metaData.imageAdded))
+          Files.deleteIfExists(path)
         }.recover {
           case NonFatal(e) =>
             SbxLog.error("Directory", s"Could not add file ${Paths.get(watchedDirectory.path).relativize(path)}: ${e.getMessage}")
